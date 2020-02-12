56d7a48e7476671ef3b49e99fcbb097b1637e0c8
cocoapi <eksrns22tp@gmail.com>
2020-02-12 22:24:27 +0900

    add depenencies #1
    resizable & dragable

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import GridLayout from 'react-grid-layout'
+// import logo from './logo.svg';
+import '../node_modules/react-grid-layout/css/styles.css'
+import '../node_modules/react-resizable/css/styles.css'
 import './App.css';
 
 function App() {
+  const layout = [
+    {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+    {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+    {i: 'c', x: 4, y: 0, w: 1, h: 2}
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header> this is header </header>
+      <nav> this is nav </nav>
+      <article> 
+        <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+          <div key="a">a</div>
+          <div key="b">b</div>
+          <div key="c">c</div>
+        </GridLayout>
+      </article>
+      <footer> this is footer </footer>
     </div>
   );
 }
